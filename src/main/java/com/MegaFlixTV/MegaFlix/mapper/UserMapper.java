9fb2337bea8f4c0991a167342aa644d72941bf1e
Kellyson <gilsonvigilante@outlook.com>
2026-03-19 00:48:21 -0300

    feat:Criado mapper para conversão dos DTOS request e response.

--- src/main/java/com/MegaFlixTV/MegaFlix/mapper/UserMapper.java
+++ src/main/java/com/MegaFlixTV/MegaFlix/mapper/UserMapper.java
@@ -1,4 +1,30 @@
 package com.MegaFlixTV.MegaFlix.mapper;
 
+import com.MegaFlixTV.MegaFlix.controller.request.UserRequest;
+import com.MegaFlixTV.MegaFlix.controller.response.UserResponse;
+import com.MegaFlixTV.MegaFlix.entity.User;
+import lombok.experimental.UtilityClass;
+
+@UtilityClass
 public class UserMapper {
+
+    public static UserResponse mapToResponse (User user) {
+
+        return UserResponse.builder()
+                .id(user.getId())
+                .user(user.getUser())
+                .password(user.getPassword())
+                .email(user.getEmail())
+                .build();
+    }
+
+    public static User mapToEntity (UserRequest userRequest) {
+
+        return User.builder()
+                .user(userRequest.user())
+                .password(userRequest.password())
+                .email(userRequest.email())
+                .build();
+    }
+
 }
