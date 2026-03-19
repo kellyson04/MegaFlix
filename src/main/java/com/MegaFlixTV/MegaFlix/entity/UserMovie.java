9220bf5f36f3355586a434ff9b5301dbf1b72da2
Kellyson <gilsonvigilante@outlook.com>
2026-03-19 08:43:42 -0300

    feat:Criada entidade para tabela de relacionamento entre usuario e filmes

--- src/main/java/com/MegaFlixTV/MegaFlix/entity/UserMovie.java
+++ src/main/java/com/MegaFlixTV/MegaFlix/entity/UserMovie.java
@@ -1,4 +1,33 @@
 package com.MegaFlixTV.MegaFlix.entity;
 
+import jakarta.persistence.*;
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+@Entity
+@Table(name = "users_movies")
+@Data
+@NoArgsConstructor
+@AllArgsConstructor
 public class UserMovie {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+
+    @ManyToOne
+    @JoinColumn(name = "user_id",nullable = false)
+    private User userId;
+
+    @ManyToOne
+    @JoinColumn(name = "movie_id",nullable = false)
+    private Movie movieId;
+
+    @Column(name = "favorite",nullable = false)
+    private boolean favorite;
+
+    @Column(name = "watched", nullable = false)
+    private boolean watched;
+
 }
