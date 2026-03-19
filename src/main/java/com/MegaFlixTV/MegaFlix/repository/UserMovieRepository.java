3e40bbf7b8860f427b59c5aad6bfa860c8d9aa5e
Kellyson <gilsonvigilante@outlook.com>
2026-03-19 08:44:21 -0300

    feat:Criado repository para tabela de relacionamento entre usuarios e filmes

--- src/main/java/com/MegaFlixTV/MegaFlix/repository/UserMovieRepository.java
+++ src/main/java/com/MegaFlixTV/MegaFlix/repository/UserMovieRepository.java
@@ -1,4 +1,7 @@
 package com.MegaFlixTV.MegaFlix.repository;
 
-public interface UserMovieRepository {
+import com.MegaFlixTV.MegaFlix.entity.UserMovie;
+import org.springframework.data.jpa.repository.JpaRepository;
+
+public interface UserMovieRepository extends JpaRepository<UserMovie,Long> {
 }
