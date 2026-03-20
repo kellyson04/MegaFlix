240212e7e407b304fe84e65a79d73f33a17b1c48
Kellyson <gilsonvigilante@outlook.com>
2026-03-19 22:53:44 -0300

    refactor:Mudança no nome userId e movieId para user e movie na classe entity ocasionou na mudança no mapper tb

--- src/main/java/com/MegaFlixTV/MegaFlix/mapper/UserMovieMapper.java
+++ src/main/java/com/MegaFlixTV/MegaFlix/mapper/UserMovieMapper.java
@@ -1,4 +1,31 @@
 package com.MegaFlixTV.MegaFlix.mapper;
 
+import com.MegaFlixTV.MegaFlix.controller.request.UserMovieRequest;
+import com.MegaFlixTV.MegaFlix.controller.request.UserRequest;
+import com.MegaFlixTV.MegaFlix.controller.response.UserMovieResponse;
+import com.MegaFlixTV.MegaFlix.entity.UserMovie;
+import lombok.experimental.UtilityClass;
+
+@UtilityClass
 public class UserMovieMapper {
+
+    public static UserMovie mapToEntity (UserMovieRequest userMovieRequest) {
+
+        return UserMovie.builder()
+                .user(userMovieRequest.user())
+                .movie(userMovieRequest.movie())
+                .favorite(userMovieRequest.favorite())
+                .watched(userMovieRequest.watched())
+                .build();
+    }
+
+    public static UserMovieResponse mapToResponse (UserMovie userMovie) {
+
+        return UserMovieResponse.builder()
+                .user(userMovie.getUser())
+                .movie(userMovie.getMovie())
+                .favorite(userMovie.isFavorite())
+                .watched(userMovie.isWatched())
+                .build();
+    }
 }
