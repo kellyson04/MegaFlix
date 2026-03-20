a839093e3002320bc4a2b8a951b3d09e6e3111e7
Kellyson <gilsonvigilante@outlook.com>
2026-03-19 22:49:51 -0300

    feat:Adicionada lógica de Assistir filme ou Adicionar aos Favoritos, ainda em fase inicial

--- src/main/java/com/MegaFlixTV/MegaFlix/service/UserMovieService.java
+++ src/main/java/com/MegaFlixTV/MegaFlix/service/UserMovieService.java
@@ -1,8 +1,10 @@
 package com.MegaFlixTV.MegaFlix.service;
 
+import com.MegaFlixTV.MegaFlix.controller.response.UserMovieResponse;
 import com.MegaFlixTV.MegaFlix.entity.Movie;
 import com.MegaFlixTV.MegaFlix.entity.User;
 import com.MegaFlixTV.MegaFlix.entity.UserMovie;
+import com.MegaFlixTV.MegaFlix.mapper.UserMovieMapper;
 import com.MegaFlixTV.MegaFlix.repository.MovieRepository;
 import com.MegaFlixTV.MegaFlix.repository.UserMovieRepository;
 import com.MegaFlixTV.MegaFlix.repository.UserRepository;
@@ -23,22 +25,66 @@ public class UserMovieService {
         this.movieRepository = movieRepository;
     }
 
-    public UserMovie adicionarFilmeAoUsuario (Long userId,Long movieId) {
-        User verificarUsuario = userRepository.findById(userId).orElseThrow(() -> new RuntimeException("Este Usuario não existe."));
-        Movie verificarFilme = movieRepository.findById(movieId).orElseThrow(() -> new RuntimeException("Este Filme não existe."));
+    public UserMovieResponse adicionarFilmeAoUsuario (Long user,Long movie) {
+        User verificarUsuario = userRepository.findById(user).orElseThrow(() -> new RuntimeException("Este Usuario não existe."));
+        Movie verificarFilme = movieRepository.findById(movie).orElseThrow(() -> new RuntimeException("Este Filme não existe."));
 
         UserMovie vincularUsuarioFilme = new UserMovie();
 
-        vincularUsuarioFilme.setUserId(verificarUsuario);
-        vincularUsuarioFilme.setMovieId(verificarFilme);
+        vincularUsuarioFilme.setUser(verificarUsuario);
+        vincularUsuarioFilme.setMovie(verificarFilme);
         vincularUsuarioFilme.setFavorite(false);
         vincularUsuarioFilme.setWatched(false);
 
-        return userMovieRepository.save(vincularUsuarioFilme);
+        userMovieRepository.save(vincularUsuarioFilme);
+
+        return UserMovieMapper.mapToResponse(vincularUsuarioFilme);
     }
 
-    public List<UserMovie> listarFilmesdeUsuarios () {
-        return userMovieRepository.findAll();
+    public List<UserMovieResponse> listarFilmesdeUsuarios () {
+       List<UserMovie> listaEntity = userMovieRepository.findAll();
+
+       return listaEntity.stream()
+               .map(userMovie -> UserMovieMapper.mapToResponse(userMovie))
+               .toList();
+    }
+
+    public UserMovieResponse listarRelacaoEspecifica (Long id) {
+        UserMovie userMovie = userMovieRepository.findById(id).orElseThrow(() -> new RuntimeException("Esta relação não existe."));
+
+        return UserMovieMapper.mapToResponse(userMovie);
+    }
+
+    public void deletarRelacao (Long id) {
+        UserMovie userMovie = userMovieRepository.findById(id).orElseThrow(() -> new RuntimeException("Esta relação não existe."));
+
+        userMovieRepository.deleteById(id);
+    }
+
+    public void adicionarFavorito (Long userId,Long movieId,Long relationId) {
+        User user = userRepository.findById(userId).orElseThrow(() -> new RuntimeException("Este usuario não existe"));
+
+        Movie movie = movieRepository.findById(movieId).orElseThrow(() -> new RuntimeException("Este filme não existe"));
+
+        UserMovie userMovie = userMovieRepository.findById(relationId).orElseThrow(() -> new RuntimeException("Filme ou usuario não possuem relação para favoritar."));
+
+        userMovie.setFavorite(true);
+
+        userMovieRepository.save(userMovie);
+    }
+
+    public UserMovieResponse assistirFilme (Long userId,Long movieId,Long relationId) {
+        User user = userRepository.findById(userId).orElseThrow(() -> new RuntimeException("Este usuario não existe"));
+
+        Movie movie = movieRepository.findById(movieId).orElseThrow(() -> new RuntimeException("Este filme não existe"));
+
+        UserMovie userMovie = userMovieRepository.findById(relationId).orElseThrow(() -> new RuntimeException("Usuario não possui o filme na playlist"));
+
+        userMovie.setWatched(true);
+
+        userMovieRepository.save(userMovie);
+
+        return UserMovieMapper.mapToResponse(userMovie);
     }
 }
 
