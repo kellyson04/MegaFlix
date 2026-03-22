9371208e10417ec421c99d792286945aa76c0ac4
Kellyson <gilsonvigilante@outlook.com>
2026-03-22 11:53:39 -0300

    feat:Criada querys para filtragem por genero e duração

--- src/main/java/com/MegaFlixTV/MegaFlix/repository/MovieRepository.java
+++ src/main/java/com/MegaFlixTV/MegaFlix/repository/MovieRepository.java
@@ -4,6 +4,10 @@ import com.MegaFlixTV.MegaFlix.entity.Movie;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
 
+import java.util.List;
+
 @Repository
 public interface MovieRepository extends JpaRepository<Movie,Long> {
+    List<Movie> findMovieByGenre(String genre);
+    List<Movie> findMovieByDuration(double duration);
 }
