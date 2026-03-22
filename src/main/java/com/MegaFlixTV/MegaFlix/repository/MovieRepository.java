4e83083ba1fbd9050ab21ff31401379e34205bd2
Kellyson <gilsonvigilante@outlook.com>
2026-03-22 13:33:36 -0300

    feat: melhora do metodo de busca pela duração do filme e cria método para buscar filmes usando containing facilitando caso usuario nao saiba nome completo do filme de determinada trilogia

--- src/main/java/com/MegaFlixTV/MegaFlix/repository/MovieRepository.java
+++ src/main/java/com/MegaFlixTV/MegaFlix/repository/MovieRepository.java
@@ -9,5 +9,7 @@ import java.util.List;
 @Repository
 public interface MovieRepository extends JpaRepository<Movie,Long> {
     List<Movie> findMovieByGenre(String genre);
-    List<Movie> findMovieByDuration(double duration);
+    List<Movie> findMovieByDurationGreaterThanEqual(double duration);
+    List<Movie> findMovieByDurationLessThanEqual(double duration);
+    List<Movie> findMovieByMovieContainingIgnoreCase (String title);
 }
