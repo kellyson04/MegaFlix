a851ba5db550cd54b6ce36a672753f9e350381bf
Kellyson <gilsonvigilante@outlook.com>
2026-03-23 14:31:14 -0300

    feat:criada classe para tratamento de exceções, ainda em fase inicial

--- src/main/java/com/MegaFlixTV/MegaFlix/exception/GlobalExceptionHandler.java
+++ src/main/java/com/MegaFlixTV/MegaFlix/exception/GlobalExceptionHandler.java
@@ -1,4 +1,27 @@
 package com.MegaFlixTV.MegaFlix.exception;
 
+import com.MegaFlixTV.MegaFlix.entity.User;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.HttpStatusCode;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.ExceptionHandler;
+import org.springframework.web.bind.annotation.RestControllerAdvice;
+
+@RestControllerAdvice
 public class GlobalExceptionHandler {
+
+    @ExceptionHandler(RuntimeException.class)
+    public ResponseEntity<String> runTimeExceptionHandler (RuntimeException e) {
+        return ResponseEntity.status(HttpStatus.NOT_FOUND).body(e.getMessage());
+    }
+
+    @ExceptionHandler(MovieNotFoundException.class)
+    public ResponseEntity<String> movieNotFoundExceptionHandler (MovieNotFoundException e) {
+        return ResponseEntity.status(HttpStatus.NOT_FOUND).body(e.getMessage());
+    }
+
+    @ExceptionHandler(UserNotFoundException.class)
+    public ResponseEntity<String> userNotFoundExceptionHandler (UserNotFoundException e) {
+        return ResponseEntity.status(HttpStatus.NOT_FOUND).body(e.getMessage());
+    }
 }
