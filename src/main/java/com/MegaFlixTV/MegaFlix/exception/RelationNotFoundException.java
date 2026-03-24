9cf20c38bd2db9c8d761115f62c726ffc4644a78
Kellyson <gilsonvigilante@outlook.com>
2026-03-24 15:47:06 -0300

    feat:criada exceção pra captar usuario e filmes nao relacionados

--- src/main/java/com/MegaFlixTV/MegaFlix/exception/RelationNotFoundException.java
+++ src/main/java/com/MegaFlixTV/MegaFlix/exception/RelationNotFoundException.java
@@ -1,7 +1,7 @@
 package com.MegaFlixTV.MegaFlix.exception;
 
 public class RelationNotFoundException extends RuntimeException {
-  public RelationNotFoundException(String message) {
-    super(message);
-  }
+    public RelationNotFoundException(String message) {
+        super(message);
+    }
 }
