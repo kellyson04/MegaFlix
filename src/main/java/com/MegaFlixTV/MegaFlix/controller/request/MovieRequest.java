20c6bc5ef76de6606f4c60fa4abadfdf5f58dc88
Kellyson <gilsonvigilante@outlook.com>
2026-03-24 15:53:20 -0300

    feat:adicionadas annotations de validação na requisição do filme

--- src/main/java/com/MegaFlixTV/MegaFlix/controller/request/MovieRequest.java
+++ src/main/java/com/MegaFlixTV/MegaFlix/controller/request/MovieRequest.java
@@ -1,9 +1,15 @@
 package com.MegaFlixTV.MegaFlix.controller.request;
 
+import jakarta.validation.constraints.*;
+
 public record MovieRequest(
         Long id,
+        @NotBlank(message = "O nome do filme não pode estar vazio")
         String movie,
+        @NotBlank(message = "O genero não pode estar vazio")
         String genre,
-        double duration
+        @NotNull(message = "A duração do filme não pode estar vazia")
+        @DecimalMin(value = "60.0", inclusive = true,message = "O tamanho do filme deve ter no minimo 1 hora de duração.")
+        Double duration
 ) {
 }
