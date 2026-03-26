20ea79d79be39eb81ff413894c6c29ea6c5ff656
Kellyson <gilsonvigilante@outlook.com>
2026-03-26 09:12:45 -0300

    feat:cria exceção de streaming não encontrado

--- src/main/java/com/MegaFlixTV/MegaFlix/exception/StreamingNotFoundException.java
+++ src/main/java/com/MegaFlixTV/MegaFlix/exception/StreamingNotFoundException.java
@@ -1,7 +1,7 @@
 package com.MegaFlixTV.MegaFlix.exception;
 
 public class StreamingNotFoundException extends RuntimeException {
-  public StreamingNotFoundException(String message) {
-    super(message);
-  }
+    public StreamingNotFoundException(String message) {
+        super(message);
+    }
 }
