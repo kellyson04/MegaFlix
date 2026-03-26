fc820b29d4f4cd546c08be3fed9233af724d5f38
Kellyson <gilsonvigilante@outlook.com>
2026-03-26 14:15:35 -0300

    feat:cria lógica de serviço para adicionar filme em algum streaming e para listar Streamings que possuem o filme

--- src/main/java/com/MegaFlixTV/MegaFlix/service/MovieService.java
+++ src/main/java/com/MegaFlixTV/MegaFlix/service/MovieService.java
@@ -2,10 +2,15 @@ package com.MegaFlixTV.MegaFlix.service;
 
 import com.MegaFlixTV.MegaFlix.controller.request.MovieRequest;
 import com.MegaFlixTV.MegaFlix.controller.response.MovieResponse;
+import com.MegaFlixTV.MegaFlix.controller.response.StreamingResponse;
 import com.MegaFlixTV.MegaFlix.entity.Movie;
+import com.MegaFlixTV.MegaFlix.entity.Streaming;
 import com.MegaFlixTV.MegaFlix.exception.MovieNotFoundException;
+import com.MegaFlixTV.MegaFlix.exception.StreamingNotFoundException;
 import com.MegaFlixTV.MegaFlix.mapper.MovieMapper;
+import com.MegaFlixTV.MegaFlix.mapper.StreamingMapper;
 import com.MegaFlixTV.MegaFlix.repository.MovieRepository;
+import com.MegaFlixTV.MegaFlix.repository.StreamingRepository;
 import org.springframework.stereotype.Service;
 
 import javax.swing.text.html.Option;
@@ -17,9 +22,11 @@ import java.util.stream.Collectors;
 @Service
 public class MovieService {
     private final MovieRepository movieRepository;
+    private final StreamingRepository streamingRepository;
 
-    public MovieService(MovieRepository movieRepository) {
+    public MovieService(MovieRepository movieRepository, StreamingRepository streamingRepository) {
         this.movieRepository = movieRepository;
+        this.streamingRepository = streamingRepository;
     }
 
     public List<MovieResponse> listarFilmes () {
@@ -104,5 +111,27 @@ public class MovieService {
 
             return listarPeloTitulo;
         }
+    }
+
+    public MovieResponse adicionarFilmeNoStreaming (Long movieId,Long streamingId) {
+        Movie movie = movieRepository.findById(movieId).orElseThrow(() -> new MovieNotFoundException("Filme não encontrado!"));
+        Streaming streaming = streamingRepository.findById(streamingId).orElseThrow(() -> new StreamingNotFoundException("Streaming não encontrado"));
+
+        streaming.getMovie().add(movie);
+
+        streamingRepository.save(streaming);
+
+
+        return MovieMapper.toResponse(movie);
+    }
+
+    public List<StreamingResponse> StreamingsQueOFilmeSeEncontra(Long movieId) {
+        Movie movie = movieRepository.findById(movieId).orElseThrow(() -> new MovieNotFoundException("Filme não existe"));
+
+
+        return movie.getStreaming()
+                .stream()
+                .map(cadaStreaming -> StreamingMapper.toResponse(cadaStreaming))
+                .toList();
     }
 }
