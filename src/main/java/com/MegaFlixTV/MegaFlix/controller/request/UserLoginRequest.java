aa6faaa1291b6cee529f8afd28b50b8780e21a98
Kellyson <gilsonvigilante@outlook.com>
2026-03-28 12:01:54 -0300

    feat:criado records para request do login do usuario

--- src/main/java/com/MegaFlixTV/MegaFlix/controller/request/UserLoginRequest.java
+++ src/main/java/com/MegaFlixTV/MegaFlix/controller/request/UserLoginRequest.java
@@ -1,4 +1,16 @@
 package com.MegaFlixTV.MegaFlix.controller.request;
 
-public record UserLoginRequest() {
+import jakarta.validation.constraints.NotBlank;
+import jakarta.validation.constraints.Pattern;
+import jakarta.validation.constraints.Size;
+
+public record UserLoginRequest(
+        @NotBlank
+        @Size(min = 2, max = 50 , message = "O nome deve ter no minimo 2 caracteres e no maximo 50")
+        String user,
+        @NotBlank
+        @Size(min = 8, max = 100)
+        @Pattern(regexp = "^(?=.*[A-Z])(?=(?:.*\\d){2,})(?=.*[^a-zA-Z0-9]).{8,}$", message = "A senha deve ter no minimo 8 caracteres 1 letra maiuscula, 2 numeros e 1 simbolo.")
+        String password
+) {
 }
