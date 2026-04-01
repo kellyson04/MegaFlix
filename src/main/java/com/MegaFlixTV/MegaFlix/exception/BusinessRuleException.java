935a538e7c94ccc8c082722e685901017f750186
Kellyson <gilsonvigilante@outlook.com>
2026-03-31 22:44:01 -0300

    feat:cria exceção pra utilizar em alguns erros q n precisam de exceptions especificas

--- src/main/java/com/MegaFlixTV/MegaFlix/exception/BusinessRuleException.java
+++ src/main/java/com/MegaFlixTV/MegaFlix/exception/BusinessRuleException.java
@@ -1,7 +1,7 @@
 package com.MegaFlixTV.MegaFlix.exception;
 
 public class BusinessRuleException extends RuntimeException {
-  public BusinessRuleException(String message) {
-    super(message);
-  }
+    public BusinessRuleException(String message) {
+        super(message);
+    }
 }
