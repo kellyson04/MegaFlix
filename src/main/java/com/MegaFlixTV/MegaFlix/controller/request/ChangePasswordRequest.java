84810a817b04dcb09de38c98543de57eefde1744
Kellyson <gilsonvigilante@outlook.com>
2026-03-31 22:45:26 -0300

    feat:cria dto para troca de senha

--- src/main/java/com/MegaFlixTV/MegaFlix/controller/request/ChangePasswordRequest.java
+++ src/main/java/com/MegaFlixTV/MegaFlix/controller/request/ChangePasswordRequest.java
@@ -1,4 +1,20 @@
 package com.MegaFlixTV.MegaFlix.controller.request;
 
-public record ChangePasswordRequest() {
+import jakarta.validation.constraints.NotBlank;
+import jakarta.validation.constraints.Pattern;
+import jakarta.validation.constraints.Size;
+
+public record ChangePasswordRequest(
+        @NotBlank(message = "Nome do usuario vazio")
+        @Size(min = 2, max = 50, message = "O nome deve ter no minimo 2 caracteres e no maximo 50")
+        String user,
+        @NotBlank
+        @Size(min = 8, max = 100)
+        @Pattern(regexp = "^(?=.*[A-Z])(?=(?:.*\\d){2,})(?=.*[^a-zA-Z0-9]).{8,}$", message = "A senha deve ter no minimo 8 caracteres 1 letra maiuscula, 2 numeros e 1 simbolo.")
+        String currentPassword,
+        @NotBlank
+        @Size(min = 8, max = 100)
+        @Pattern(regexp = "^(?=.*[A-Z])(?=(?:.*\\d){2,})(?=.*[^a-zA-Z0-9]).{8,}$", message = "A senha deve ter no minimo 8 caracteres 1 letra maiuscula, 2 numeros e 1 simbolo.")
+        String newPassword
+) {
 }
