fc0f1958bf8154b95d13351dabea06a65a905bc5
Kellyson <gilsonvigilante@outlook.com>
2026-04-02 17:29:45 -0300

    feat:adiciona metodo pra especificar se determinado filme esta vinculado a determinado streaming

--- src/main/java/com/MegaFlixTV/MegaFlix/repository/MovieRepository.java
+++ src/main/java/com/MegaFlixTV/MegaFlix/repository/MovieRepository.java
@@ -1,6 +1,7 @@
 package com.MegaFlixTV.MegaFlix.repository;
 
 import com.MegaFlixTV.MegaFlix.entity.Movie;
+import com.MegaFlixTV.MegaFlix.entity.Streaming;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
 
@@ -12,4 +13,5 @@ public interface MovieRepository extends JpaRepository<Movie,Long> {
     List<Movie> findMovieByDurationGreaterThanEqual(double duration);
     List<Movie> findMovieByDurationLessThanEqual(double duration);
     List<Movie> findMovieByMovieContainingIgnoreCase (String title);
+    boolean existsByIdAndStreamingId(Long movieId,Long streamingId);
 }
