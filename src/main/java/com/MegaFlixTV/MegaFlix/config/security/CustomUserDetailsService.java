ca4154cb40d1608d40d0059a4cfd4ea080332da2
Kellyson <gilsonvigilante@outlook.com>
2026-04-03 13:59:57 -0300

    feat:implementa userdetailsservice pra autenticar o usuario e usa o repositorio de usuario pra buscar os dados

--- src/main/java/com/MegaFlixTV/MegaFlix/config/security/CustomUserDetailsService.java
+++ src/main/java/com/MegaFlixTV/MegaFlix/config/security/CustomUserDetailsService.java
@@ -1,4 +1,23 @@
 package com.MegaFlixTV.MegaFlix.config.security;
 
-public class CustomUserDetailsService {
+import com.MegaFlixTV.MegaFlix.exception.UserNotFoundException;
+import com.MegaFlixTV.MegaFlix.repository.UserRepository;
+import org.springframework.security.core.userdetails.UserDetails;
+import org.springframework.security.core.userdetails.UserDetailsService;
+import org.springframework.security.core.userdetails.UsernameNotFoundException;
+import org.springframework.stereotype.Service;
+
+@Service
+public class CustomUserDetailsService implements UserDetailsService {
+
+    private final UserRepository userRepository;
+
+    public CustomUserDetailsService(UserRepository userRepository) {
+        this.userRepository = userRepository;
+    }
+
+    @Override
+    public UserDetails loadUserByUsername(String username) throws UsernameNotFoundException {
+        return userRepository.findUserByUser(username).orElseThrow(() -> new UserNotFoundException("Usuario não encontrado"));
+    }
 }
