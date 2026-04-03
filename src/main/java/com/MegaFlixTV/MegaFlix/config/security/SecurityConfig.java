2654b8c0511e114793f37d5e4c6ff2f2625dd4e1
Kellyson <gilsonvigilante@outlook.com>
2026-04-03 14:08:52 -0300

    fix:organiza acesso dos endpoints agora q a entidade possui roles, continua com basic auth

--- src/main/java/com/MegaFlixTV/MegaFlix/config/security/SecurityConfig.java
+++ src/main/java/com/MegaFlixTV/MegaFlix/config/security/SecurityConfig.java
@@ -3,9 +3,12 @@ package com.MegaFlixTV.MegaFlix.config.security;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.http.HttpMethod;
+import org.springframework.security.authentication.AuthenticationProvider;
+import org.springframework.security.authentication.dao.DaoAuthenticationProvider;
 import org.springframework.security.config.Customizer;
 import org.springframework.security.config.annotation.method.configuration.EnableMethodSecurity;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+import org.springframework.security.core.userdetails.UserDetailsService;
 import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
 import org.springframework.security.crypto.password.PasswordEncoder;
 import org.springframework.security.web.SecurityFilterChain;
@@ -22,11 +25,12 @@ public class SecurityConfig {
                                 .requestMatchers(HttpMethod.POST,"/megaflix/users").permitAll()
                                 .requestMatchers(HttpMethod.GET,"/megaflix/users").permitAll()
                                 .requestMatchers(HttpMethod.GET,"/megaflix/users/{id}").permitAll()
-                        //esse request put abaixo tipo tava has role admin e tava impedindo de eu trocar a senha com 401 no postman, n sei pq isso, investigar amanhã
                                 .requestMatchers(HttpMethod.PUT,"/megaflix/users/{id}").permitAll()
                                 .requestMatchers(HttpMethod.DELETE,"/megaflix/users/{id}").hasRole("ADMIN")
                                 .requestMatchers(HttpMethod.POST,"/megaflix/users/login").permitAll()
-                                .requestMatchers(HttpMethod.PUT,"/megaflix/users/change-password").permitAll()
+                                .requestMatchers(HttpMethod.PUT,"/megaflix/users/change-password").authenticated()
+
+                                .requestMatchers(HttpMethod.PATCH,"/megaflix/users/{id}").authenticated()
                                 .requestMatchers(HttpMethod.POST,"/megaflix/movie").hasRole("ADMIN")
                                 .requestMatchers(HttpMethod.GET,"/megaflix/movie").permitAll()
                                 .requestMatchers(HttpMethod.GET,"/megaflix/movie/{id}").permitAll()
@@ -37,16 +41,16 @@ public class SecurityConfig {
                                 .requestMatchers(HttpMethod.GET,"/megaflix/movie/filtrar/por-duracao-menor").permitAll()
                                 .requestMatchers(HttpMethod.GET,"/megaflix/movie/filtrar/por-titulo").permitAll()
                                 .requestMatchers(HttpMethod.POST,"/megaflix/movie/streaming/{movieId}/{streamingId}").hasRole("ADMIN")
-                                .requestMatchers(HttpMethod.GET,"/megaflix/movie/{movieId}/streamings").authenticated()
-                                .requestMatchers(HttpMethod.POST,"/megaflix/playlist/{userId}/{movieId}").permitAll()
+                                .requestMatchers(HttpMethod.GET,"/megaflix/movie/{movieId}/streamings").permitAll()
+                                .requestMatchers(HttpMethod.POST,"/megaflix/playlist/{userId}/{movieId}").authenticated()
                                 .requestMatchers(HttpMethod.GET,"/megaflix/playlist").permitAll()
                                 .requestMatchers(HttpMethod.GET,"/megaflix/playlist/{id}").permitAll()
-                                .requestMatchers(HttpMethod.DELETE,"/megaflix/playlist/{id}").hasRole("ADMIN")
+                                .requestMatchers(HttpMethod.DELETE,"/megaflix/playlist/{id}").permitAll()
                                 .requestMatchers(HttpMethod.POST,"/megaflix/playlist/watch/user/{userId}/movie/{movieId}").authenticated()
-                                .requestMatchers(HttpMethod.POST,"/megaflix/playlist/favorite/{relationId}").permitAll()
-                                .requestMatchers(HttpMethod.POST,"/megaflix/playlist/unfavorite/{relationId}").permitAll()
+                                .requestMatchers(HttpMethod.POST,"/megaflix/playlist/favorite/{relationId}").authenticated()
+                                .requestMatchers(HttpMethod.POST,"/megaflix/playlist/unfavorite/{relationId}").authenticated()
                                 .requestMatchers(HttpMethod.GET,"/megaflix/playlist/favorites").permitAll()
-                                .requestMatchers(HttpMethod.GET,"/megaflix/playlist/user/{userId}/favorites").authenticated()
+                                .requestMatchers(HttpMethod.GET,"/megaflix/playlist/user/{userId}/favorites").permitAll()
                                 .requestMatchers(HttpMethod.POST,"/megaflix/streaming").hasRole("ADMIN")
                                 .requestMatchers(HttpMethod.GET,"/megaflix/streaming").permitAll()
                                 .requestMatchers(HttpMethod.GET,"/megaflix/streaming/{id}").permitAll()
@@ -65,4 +69,6 @@ public class SecurityConfig {
     public PasswordEncoder passwordEncoder () {
         return new BCryptPasswordEncoder();
     }
+
+
 }
