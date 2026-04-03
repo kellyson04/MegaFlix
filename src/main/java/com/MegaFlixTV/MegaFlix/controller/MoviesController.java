98f93bd4106b083557b1d57fee0f7740ce1d9acc
Kellyson <gilsonvigilante@outlook.com>
2026-04-03 18:15:04 -0300

    refactor:deleta endpoints de filtragem e faz filtragem no mesmo endpoint de listagem,muda rota para movies e organiza padrao REST nos endpoints

--- src/main/java/com/MegaFlixTV/MegaFlix/controller/MoviesController.java
+++ src/main/java/com/MegaFlixTV/MegaFlix/controller/MoviesController.java
@@ -12,7 +12,7 @@ import org.springframework.web.bind.annotation.*;
 import java.util.List;
 
 @RestController
-@RequestMapping("/megaflix/movie")
+@RequestMapping("/megaflix/movies")
 public class MoviesController {
     private final MovieService movieService;
 
@@ -27,12 +27,14 @@ public class MoviesController {
 
 
     @GetMapping()
-    public ResponseEntity<List<MovieResponse>> listarFilmes () {
-        return ResponseEntity.ok(movieService.listarFilmes());
+    public ResponseEntity<List<MovieResponse>> listarFilmes (@RequestParam(required = false) String genero,
+                                                             @RequestParam(required = false) String titulo,
+                                                             @RequestParam(required = false) Double duracaoMaior,
+                                                             @RequestParam(required = false) Double duracaoMenor) {
+        return ResponseEntity.ok(movieService.listarFilmes(genero,titulo,duracaoMaior,duracaoMenor));
     }
 
-
-    @GetMapping("{id}")
+    @GetMapping("/{id}")
     public ResponseEntity<MovieResponse> listarFilmePorId (@PathVariable Long id) {
         return ResponseEntity.ok(movieService.listarFilmeEspecifico(id));
     }
@@ -50,29 +52,7 @@ public class MoviesController {
         return ResponseEntity.status(HttpStatus.NO_CONTENT).build();
     }
 
-    @GetMapping("/filtrar/por-genero")
-    public ResponseEntity <List<MovieResponse>> ListarPorGenero (@RequestParam String genero) {
-
-        return ResponseEntity.ok(movieService.listarFilmesPorGenero(genero));
-    }
-
-    @GetMapping("/filtrar/por-duracao-maior")
-    public ResponseEntity<List<MovieResponse>> listarPorDuracaoMaiorOuIgual (@RequestParam double duracaoMaior) {
-        return ResponseEntity.ok(movieService.listarFilmesPelaDuracaoMaior(duracaoMaior));
-    }
-
-    @GetMapping("/filtrar/por-duracao-menor")
-    public ResponseEntity<List<MovieResponse>> listarPorDuracaoMenorOuIgual (@RequestParam double duracaoMenor) {
-        return ResponseEntity.ok(movieService.listarFilmesPelaDuracaoMenor(duracaoMenor));
-    }
-
-    @GetMapping("/filtrar/por-titulo")
-    public ResponseEntity<List<MovieResponse>> listarPorTitulo (@RequestParam String titulo) {
-
-        return ResponseEntity.ok(movieService.listarFilmesPeloTitulo(titulo));
-    }
-
-    @PostMapping("/streaming/{movieId}/{streamingId}")
+    @PostMapping("/{movieId}/streaming/{streamingId}")
     public ResponseEntity<MovieResponse> adicionarVinculoComStreaming (@PathVariable Long movieId,@PathVariable Long streamingId) {
         return ResponseEntity.status(HttpStatus.CREATED).body(movieService.adicionarFilmeNoStreaming(movieId,streamingId));
     }
