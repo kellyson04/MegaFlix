9569f7fcd5cbe1159aafd238593abb012816f0e0
Kellyson <gilsonvigilante@outlook.com>
2026-04-03 14:12:25 -0300

    feat:adiciona validação no dto

--- src/main/java/com/MegaFlixTV/MegaFlix/controller/request/StreamingRequest.java
+++ src/main/java/com/MegaFlixTV/MegaFlix/controller/request/StreamingRequest.java
@@ -1,4 +1,10 @@
 package com.MegaFlixTV.MegaFlix.controller.request;
 
-public record StreamingRequest (Long id, String name){
+import jakarta.validation.constraints.NotBlank;
+import jakarta.validation.constraints.Size;
+
+public record StreamingRequest (Long id,
+                                @NotBlank
+                                @Size(min = 2, max = 50, message = "O nome do streaming tem que ter entre 2 a 50 caracteres.")
+                                String name){
 }
