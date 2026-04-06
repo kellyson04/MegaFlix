fdcaeaff198a354636a643efb28f5b221a737cf4
Kellyson <gilsonvigilante@outlook.com>
2026-04-06 19:29:50 -0300

    feat:adiciona releasyYear e rating no Request

--- src/main/java/com/MegaFlixTV/MegaFlix/controller/request/MovieRequest.java
+++ src/main/java/com/MegaFlixTV/MegaFlix/controller/request/MovieRequest.java
@@ -10,6 +10,14 @@ public record MovieRequest(
         String genre,
         @NotNull(message = "A duração do filme não pode estar vazia")
         @DecimalMin(value = "60.0", inclusive = true,message = "O tamanho do filme deve ter no minimo 1 hora de duração.")
-        Double duration
+        Double duration,
+        @NotNull(message = "Ano de lançamento não pode estar vazio")
+        @Min(1910)
+        @Max(2026)
+        Integer release_year,
+        @NotNull(message = "Nota do filme não pode estar vazia")
+        @DecimalMin(value = "0.0",inclusive = true, message = "A nota do filme não pode ser negativa")
+        @DecimalMax(value = "10.0",inclusive = true,message = "A nota do filme não pode ultrapassar 10")
+        Double rating
 ) {
 }
