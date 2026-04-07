0cb3693a8b2ad26cd2048fc019c173b9fc3e9863
Kellyson <gilsonvigilante@outlook.com>
2026-04-07 20:09:45 -0300

    feat:DTO pra Patch de filmes

--- src/main/java/com/MegaFlixTV/MegaFlix/controller/request/MoviePatchRequest.java
+++ src/main/java/com/MegaFlixTV/MegaFlix/controller/request/MoviePatchRequest.java
@@ -1,4 +1,18 @@
 package com.MegaFlixTV.MegaFlix.controller.request;
 
-public record MoviePatchRequest() {
+import jakarta.validation.constraints.*;
+
+public record MoviePatchRequest(
+
+        String movie,
+        String genre,
+        @DecimalMin(value = "60.0", inclusive = true,message = "O tamanho do filme deve ter no minimo 1 hora de duração.")
+        Double duration,
+        @Min(1910)
+        @Max(2026)
+        Integer releaseYear,
+        @DecimalMin(value = "0.0",inclusive = true, message = "A nota do filme não pode ser negativa")
+        @DecimalMax(value = "10.0",inclusive = true,message = "A nota do filme não pode ultrapassar 10")
+        Double rating
+) {
 }
