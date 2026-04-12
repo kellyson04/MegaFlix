2790af18b58e6b7ae05448452af25421e60a765d
Kellyson <gilsonvigilante@outlook.com>
2026-04-12 13:20:09 -0300

    fix:remove atributo currentPassword e move pra pasta de DTOs do user

--- src/main/java/com/MegaFlixTV/MegaFlix/controller/request/userRequests/ChangeUserDataRequest.java
+++ src/main/java/com/MegaFlixTV/MegaFlix/controller/request/userRequests/ChangeUserDataRequest.java
@@ -1,15 +1,11 @@
-package com.MegaFlixTV.MegaFlix.controller.request;
+package com.MegaFlixTV.MegaFlix.controller.request.userRequests;
 
 import jakarta.validation.constraints.Email;
-import jakarta.validation.constraints.NotBlank;
 import jakarta.validation.constraints.Pattern;
 import jakarta.validation.constraints.Size;
 
 public record ChangeUserDataRequest(
         String username,
-        @Size(min = 8, max = 100)
-        @Pattern(regexp = "^(?=.*[A-Z])(?=(?:.*\\d){2,})(?=.*[^a-zA-Z0-9]).{8,}$", message = "A senha deve ter no minimo 8 caracteres 1 letra maiuscula, 2 numeros e 1 simbolo.")
-        String currentPassword,
         @Email
         String email
 ) {
