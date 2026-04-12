4bcb1a326dce58455eef7fcdcda033ea1d355eff
Kellyson <gilsonvigilante@outlook.com>
2026-04-12 13:24:35 -0300

    feat:cria novo metodo de mapper pra troca de dados do usuario e adiciona role pra seguir padrao de sempre ser user

--- src/main/java/com/MegaFlixTV/MegaFlix/mapper/UserMapper.java
+++ src/main/java/com/MegaFlixTV/MegaFlix/mapper/UserMapper.java
@@ -1,27 +1,35 @@
 package com.MegaFlixTV.MegaFlix.mapper;
 
-import com.MegaFlixTV.MegaFlix.controller.request.UserRequest;
+import com.MegaFlixTV.MegaFlix.controller.request.userRequests.ChangeUserDataRequest;
+import com.MegaFlixTV.MegaFlix.controller.request.userRequests.CreateUserRequest;
 import com.MegaFlixTV.MegaFlix.controller.response.UserResponse;
 import com.MegaFlixTV.MegaFlix.entity.User;
+import com.MegaFlixTV.MegaFlix.entity.UserRole;
 import lombok.experimental.UtilityClass;
 
 @UtilityClass
 public class UserMapper {
 
     public static UserResponse mapToResponse (User user) {
-
         return UserResponse.builder()
                 .id(user.getId())
                 .username(user.getUsername())
                 .build();
     }
 
-    public static User mapToEntity (UserRequest userRequest) {
-
+    public static User mapToEntity (CreateUserRequest createUserRequest) {
         return User.builder()
-                .username(userRequest.username())
-                .password(userRequest.password())
-                .email(userRequest.email())
+                .username(createUserRequest.username())
+                .password(createUserRequest.password())
+                .email(createUserRequest.email())
+                .role(UserRole.ROLE_USER)
+                .build();
+    }
+
+    public static User mapChangesToEntity (ChangeUserDataRequest changeUserDataRequest) {
+        return User.builder()
+                .username(changeUserDataRequest.username())
+                .email(changeUserDataRequest.email())
                 .build();
     }
 
