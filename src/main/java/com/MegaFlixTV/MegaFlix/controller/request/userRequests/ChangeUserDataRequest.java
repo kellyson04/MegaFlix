53646ffe28c082d5e75c1d0126e9c57a706f4051
Kellyson <gilsonvigilante@outlook.com>
2026-04-12 13:18:30 -0300

    refactor:move pra pasta de DTOs do user

--- src/main/java/com/MegaFlixTV/MegaFlix/controller/request/userRequests/ChangeUserDataRequest.java
+++ src/main/java/com/MegaFlixTV/MegaFlix/controller/request/userRequests/ChangeUserDataRequest.java
@@ -5,15 +5,11 @@ import jakarta.validation.constraints.NotBlank;
 import jakarta.validation.constraints.Pattern;
 import jakarta.validation.constraints.Size;
 
-public record UserRequest(
-        @NotBlank(message = "Nome do usuario vazio")
-        @Size(min = 2, max = 50, message = "O nome deve ter no minimo 2 caracteres e no maximo 50")
+public record ChangeUserDataRequest(
         String username,
-        @NotBlank
         @Size(min = 8, max = 100)
         @Pattern(regexp = "^(?=.*[A-Z])(?=(?:.*\\d){2,})(?=.*[^a-zA-Z0-9]).{8,}$", message = "A senha deve ter no minimo 8 caracteres 1 letra maiuscula, 2 numeros e 1 simbolo.")
-        String password,
-        @NotBlank
+        String currentPassword,
         @Email
         String email
 ) {
--- src/main/java/com/MegaFlixTV/MegaFlix/controller/request/userRequests/CreateUserRequest.java
+++ src/main/java/com/MegaFlixTV/MegaFlix/controller/request/userRequests/CreateUserRequest.java
@@ -1,20 +1,18 @@
 package com.MegaFlixTV.MegaFlix.controller.request;
 
+import jakarta.validation.constraints.Email;
 import jakarta.validation.constraints.NotBlank;
 import jakarta.validation.constraints.Pattern;
 import jakarta.validation.constraints.Size;
 
-public record ChangePasswordRequest(
-        @NotBlank(message = "Nome do usuario vazio")
+public record UserRequest(
+
         @Size(min = 2, max = 50, message = "O nome deve ter no minimo 2 caracteres e no maximo 50")
-        String user,
-        @NotBlank
+        String username,
         @Size(min = 8, max = 100)
         @Pattern(regexp = "^(?=.*[A-Z])(?=(?:.*\\d){2,})(?=.*[^a-zA-Z0-9]).{8,}$", message = "A senha deve ter no minimo 8 caracteres 1 letra maiuscula, 2 numeros e 1 simbolo.")
-        String currentPassword,
-        @NotBlank
-        @Size(min = 8, max = 100)
-        @Pattern(regexp = "^(?=.*[A-Z])(?=(?:.*\\d){2,})(?=.*[^a-zA-Z0-9]).{8,}$", message = "A senha deve ter no minimo 8 caracteres 1 letra maiuscula, 2 numeros e 1 simbolo.")
-        String newPassword
+        String password,
+        @Email
+        String email
 ) {
 }
