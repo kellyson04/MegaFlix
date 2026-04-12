ab95e625ed1d655475fafca587fcc11f4f937408
Kellyson <gilsonvigilante@outlook.com>
2026-04-12 13:22:14 -0300

    fix:corrige erro do metodo getUsername q ficava se chamando infinitamente gerando stackoverflow

--- src/main/java/com/MegaFlixTV/MegaFlix/entity/User.java
+++ src/main/java/com/MegaFlixTV/MegaFlix/entity/User.java
@@ -44,7 +44,7 @@ public class User implements UserDetails {
 
     @Override
     public String getUsername() {
-        return this.getUsername();
+        return this.username;
     }
 
     @Override
